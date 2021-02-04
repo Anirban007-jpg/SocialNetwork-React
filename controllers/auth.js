9847a30b1494b9fc035a028fe361f33195f0c2fc
Anirban Banerjee <abanerjee763@outlook.com>
2021-02-04 21:46:34 +0530

    Added User Sign out Controller function and  Routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,3 +42,11 @@ exports.signin = (req, res) => {
     });
     
 }
+
+
+exports.signout = (req, res) => {
+    res.clearCookie("t");
+    return res.json({
+        message: "Signout success!"
+    })
+}
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
-const {signup, signin} = require('../controllers/auth');
+const {signup, signin, signout} = require('../controllers/auth');
 const express  = require('express');
 const {userSignupValidator} = require('../helpers/index');
+
 
 const router = express.Router();
 
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
+router.get("/signout", signout)
 
 module.exports = router;
