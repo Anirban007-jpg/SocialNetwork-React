429369260e538a3d317b14badd52bf6a8b9020c6
Anirban Banerjee <abanerjee763@outlook.com>
2021-02-04 14:59:41 +0530

    Added User schema

--- routes/post.js
+++ routes/post.js
@@ -1,11 +1,11 @@
-const postController = require('../controllers/post');
+const {getPosts, createPost} = require('../controllers/post');
 const express  = require('express');
-const validator = require('../helpers/index')
+const {createPostValidator} = require('../helpers/index');
 
 const router = express.Router();
 
-router.get('/', postController.getPosts)
-router.post('/post',  validator.createPostValidator, postController.createPost)
+router.get('/', getPosts)
+router.post('/post',  createPostValidator, createPost)
 
 module.exports = router;
 // exports.getPosts = (req, res) => {
