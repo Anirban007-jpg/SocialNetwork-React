17d71ce74c9ba5ab04e215b00b94353aef81dfd8
Anirban Banerjee <abanerjee763@outlook.com>
2021-02-05 12:19:44 +0530

    Added Authorization Step 1

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const {signup, signin, signout} = require('../controllers/auth');
+const {userById} = require('../controllers/user');
 const express  = require('express');
 const {userSignupValidator} = require('../helpers/index');
 
@@ -7,6 +8,10 @@ const router = express.Router();
 
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
-router.get("/signout", signout)
+router.get("/signout", signout);
+
+
+// any route containing :userId, our app will first execute userbyId
+router.param("userId", userById);
 
 module.exports = router;
--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,16 @@
 const {getPosts, createPost} = require('../controllers/post');
 const express  = require('express');
 const {createPostValidator} = require('../helpers/index');
+const {userById} = require('../controllers/user');
 const { requireSignin } = require('../controllers/auth');
 
 const router = express.Router();
 
 router.get('/', requireSignin, getPosts)
 router.post('/post',  createPostValidator, createPost)
+
+// any route containing :userId, our app will first execute userbyId
+router.param("userId", userById);
 
 module.exports = router;
 // exports.getPosts = (req, res) => {
