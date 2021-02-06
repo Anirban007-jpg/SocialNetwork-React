6ec1ccde610e99ebb272f3541b052ee801f28011
Anirban Banerjee <abanerjee763@outlook.com>
2021-02-06 21:10:11 +0530

    Added functionality to create post with image upload and user. Closes #2

--- controllers/post.js
+++ controllers/post.js
@@ -1,4 +1,6 @@
-const Post = require('../models/post')
+const Post = require('../models/post');
+const fro = require('formidable');
+const fs = require('fs');
 
 exports.getPosts = (req, res) => {
     const post = Post.find().select("_id title body")
@@ -9,18 +11,30 @@ exports.getPosts = (req, res) => {
 };
 
 exports.createPost = (req, res) => {
-    const post =  new Post(req.body);
-    //console.log("Creating posts: ", req.body);
-    post.save().then(result => {
-        res.status(200).json({
-            post: result
-        })
-    });
+    let form = new fro.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Your image could not be uploaded"
+            })
+        }
+        let post = new post(fields);
+        post.postedBy = req.profile;
+        if (files.photo) {
+            post.photo.data = fs.readFileSync(files.photo.path);
+            post.photo.contentType = files.photo.type
+        }
 
-        // if (err){
-        //     return res.status(400).json({
-        //         error: err
-        //     })
-        // };
-        
+        post.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                })
+            }
+
+            res.json(result);
+        })
+    })
+      
 }
--- models/post.js
+++ models/post.js
@@ -12,7 +12,7 @@ const postSchema = new mongoose.Schema({
     },
     photo: {
         type: Buffer,
-        contenType: String
+        contentType: String
     },
     postedBy: {
         type: ObjectId,
