0d91e46f46a1c506bf294deff195af33b088a298
Anirban Banerjee <abanerjee763@outlook.com>
2021-02-24 12:40:02 +0530

    Closes #13

--- socialmediafrontend/src/MainRouter.js
+++ socialmediafrontend/src/MainRouter.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './core/Home'; 
+import signup from './user/signup'
 
 const MainRouter = () => (
     <div>
         <Switch>
-            <Route path="/" component={Home}></Route>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/signup" component={signup}></Route>
         </Switch>
     </div>
 );
---
+++ socialmediafrontend/src/user/signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class signup extends Component {
+    state = {  }
+    render() {
+        return (
+            <h2>Signup</h2>
+        );
+    }
+}
+
+export default signup;
