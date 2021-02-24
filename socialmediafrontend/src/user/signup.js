faaa89d929710483f28102488fd9dbbad78a5ba3
Anirban Banerjee <abanerjee763@outlook.com>
2021-02-24 22:01:02 +0530

    Closes #19

--- socialmediafrontend/src/user/signup.js
+++ socialmediafrontend/src/user/signup.js
@@ -57,13 +57,33 @@ class signup extends Component {
         }).catch(err => console.log(err))
     }
 
+    signupForm = (name, email, password) => (
+        <form>
+                <div className="form-group">
+                    <label className="text-muted"><b>Name</b></label>
+                    <input onChange={this.handleChange("name")} type="text" className="form-control" placeholder="Enter your username.." value={name} />
+                </div>
+                <div className="form-group">
+                    <label className="text-muted"><b>Email</b></label>
+                    <input onChange={this.handleChange("email")} type="email" className="form-control" placeholder="Enter the Email to be registered...." value={email} />
+                </div>
+                <div className="form-group">
+                    <label className="text-muted"><b>Password</b></label>
+                    <input onChange={this.handleChange("password")} type="password" className="form-control" placeholder="Enter the password to be set..." value={password} />
+                </div>
+                <button onClick={this.clickSubmit} className="btn btn-raised btn-success">
+                    Submit
+                </button>&nbsp;
+        </form>
+    )
+
     render() {
         const {name, email, password, error, open} = this.state;
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Signup</h2>
 
-                <div className="alert alert-primary" style={{display: error ? "" : "none"}}>
+                <div className="alert alert-danger" style={{display: error ? "" : "none"}}>
                     {error}
                 </div>
 
@@ -72,23 +92,7 @@ class signup extends Component {
                 </div>
 
                     
-                    <form>
-                        <div className="form-group">
-                            <label className="text-muted"><b>Name</b></label>
-                            <input onChange={this.handleChange("name")} type="text" className="form-control" placeholder="Enter your username.." value={name} />
-                        </div>
-                        <div className="form-group">
-                            <label className="text-muted"><b>Email</b></label>
-                            <input onChange={this.handleChange("email")} type="email" className="form-control" placeholder="Enter the Email to be registered...." value={email} />
-                        </div>
-                        <div className="form-group">
-                            <label className="text-muted"><b>Password</b></label>
-                            <input onChange={this.handleChange("password")} type="password" className="form-control" placeholder="Enter the password to be set..." value={password} />
-                        </div>
-                        <button onClick={this.clickSubmit} className="btn btn-raised btn-success">
-                            Submit
-                        </button>&nbsp;
-                    </form>
+                   {this.signupForm(name, email, password)}
             </div>
         );
     }
