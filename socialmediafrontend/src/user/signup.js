9152a78c740b7f140177c0cdaa09981a5f8d4bd8
Anirban Banerjee <abanerjee763@outlook.com>
2021-02-24 12:56:53 +0530

    Closes #14

--- socialmediafrontend/src/user/signup.js
+++ socialmediafrontend/src/user/signup.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
 
 class signup extends Component {
-    state = {  }
+
+    constructor() {
+        super()
+        this.setState = {
+            name: "",
+            email: "",
+            password: "",
+            error: ""
+        }
+    }
+
     render() {
         return (
-            <h2>Signup</h2>
+            <div className="container">
+                <h2 className="mt-5 mb-5">Signup</h2>
+                    
+                    <form>
+                        <div className="form-group">
+                            <label className="text-muted">Name</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-muted">Email</label>
+                            <input type="email" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-muted">Password</label>
+                            <input type="password" className="form-control" />
+                        </div>
+                        <button className="btn btn-raised btn-success">
+                            Submit
+                        </button>&nbsp;
+                        <input type="reset" className="btn btn-raised btn-danger" value="reset" />
+                    </form>
+            </div>
         );
     }
 }
