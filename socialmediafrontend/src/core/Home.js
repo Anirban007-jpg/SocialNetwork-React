745cec5a94977d301facdd2bbf03d9ea9da36f70
Anirban Banerjee <abanerjee763@outlook.com>
2021-02-24 12:36:14 +0530

    Closes #12

---
+++ socialmediafrontend/src/core/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="jumbotron">
+          <h2>HOME</h2>
+          <p className="lead">Welcome to react Frontend</p>
+        </div>
+    );
+}
+
+export default Home;
+
