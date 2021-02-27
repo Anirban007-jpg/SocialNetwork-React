84df8e247516b1a40e4ec1f4f4fcad7467a7d68c
Anirban Banerjee <abanerjee763@outlook.com>
2021-02-27 13:32:48 +0530

    Closes #30

--- socialmediafrontend/src/user/Profile.js
+++ socialmediafrontend/src/user/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom';
 import { isAuthenticated } from '../Auth/index'
 
 class Profile extends Component {
@@ -11,15 +12,41 @@ class Profile extends Component {
     }
 
     componentDidMount() {
-        console.log("user id", this.props.match.params.userId);
+        const userId = this.props.match.params.userId;
+        fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type" : "application/json",
+                Authorization: `Bearer ${isAuthenticated().token}`
+            }
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            if (data.error) {
+                this.setState({redirectToSignin: true});
+            }
+            else {
+                this.setState({ user: data });
+            }
+        })
     }
 
     render() {
+
+        const {redirectToSignin} = this.state.redirectToSignin;
+        if (redirectToSignin){
+            return <Redirect to='/signin' />
+        }
+
         return (
             <div className="container">
                 <h2 className="mt-5 mb-5">Profile</h2>
                 <p>Hello {isAuthenticated().user.name}</p>
                 <p>Email: {isAuthenticated().user.email}</p>
+                <p>{`Joined ${new Date(this.state.user.created).toDateString()}`}</p>
             </div>
         );
     }
