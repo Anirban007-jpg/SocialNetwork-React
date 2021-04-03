ba2f8025d1d6fd7775676a3a848ed46b203750de
Anirban Banerjee <abanerjee763@outlook.com>
2021-04-03 21:19:20 +0530

    Closes #35

--- socialmediafrontend/src/user/User.js
+++ socialmediafrontend/src/user/User.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { list } from "./apiUser";
+import DefaultProfile from "../images/images.png"
 
 class Users extends Component {
     
@@ -20,21 +21,34 @@ class Users extends Component {
         })
     }
 
+    renderUsers = users => (
+        <div className="row">
+                    {users.map((user, i) => 
+                        (
+                            <div className="card col-md-4" key={i}>
+                                <img className="card-img-top" src={DefaultProfile} alt={user.name} style={{width: '100%', height: '15vw', objectFit: 'cover'}} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{user.name}</h5>
+                                    <p className="card-text">
+                                        {user.email}
+                                    </p>
+                                    <a href="#" className="btn btn-primary btn-raised btn-sm">
+                                        View Profile
+                                    </a>
+                                </div>
+                            </div>
+                        )
+                    )}
+        </div>
+    )
+
     render() {
         const {users} = this.state;
         return (
             <div  className="container">
                 <h2 className="mt-5 mb-5">Users</h2>
 
-                <div className="card">
-                    {users.map((user, i) => 
-                        (
-                            <div key={i}>
-                                {user.name}
-                                </div>
-                        )
-                    )}
-                </div>
+                {this.renderUsers(users)}
             </div>
         );
     }
