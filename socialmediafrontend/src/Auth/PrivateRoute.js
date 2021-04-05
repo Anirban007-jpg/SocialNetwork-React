7707e6688023afdc4cbbc4b121767e26fa1e85a0
Anirban Banerjee <abanerjee763@outlook.com>
2021-04-05 16:43:52 +0530

    Closes #40

--- socialmediafrontend/src/Auth/PrivateRoute.js
+++ socialmediafrontend/src/Auth/PrivateRoute.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Component } from 'react';
 import {Route, Redirect} from 'react-router-dom'
 import {isAuthenticated} from './index'
 
-const PrivateRoute = ({component: Component, ...rest}) => {
+const PrivateRoute = ({component: Component, ...rest}) => (
     // props means components passed down  to this private route
     <Route {...rest} render={props => isAuthenticated() ? (
         <Component {...props} />
     ) : (
         <Redirect to={{pathname: "/signin", state: {from: props.location}}} />
     )} />
-}
+)
 
 export default PrivateRoute;
