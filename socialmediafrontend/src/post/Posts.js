3036711e8024f7df00b708cd0ab5933628240d4f
Anirban Banerjee <abanerjee763@outlook.com>
2021-05-07 18:16:35 +0530

    Closes #50

--- socialmediafrontend/src/post/Posts.js
+++ socialmediafrontend/src/post/Posts.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { list } from "./apiPost";
-// import DefaultProfile from "../images/images.png"
+import DefaultProfile from "../images/images.png"
 import { Link } from 'react-router-dom';
 
 class Posts extends Component {
@@ -32,7 +32,7 @@ class Posts extends Component {
                             
                             return (
                                 <div className="card col-md-4" key={i}>
-                                        {/* <img src={`${process.env.REACT_APP_API_URL}/user/photo/${user._id}`} onError={i => (i.target.src = `${DefaultProfile}`)} alt={user.name} style={{height: "200px", width: "auto"}} className="img-thumbnail" /> */}
+                                        <img src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`} onError={i => (i.target.src = `${DefaultProfile}`)} alt={post.name} style={{height: "200px", width: "auto"}} className="img-thumbnail" />
                                     <div className="card-body">
                                         <h5 className="card-title">{post.title}</h5>
                                         <p className="card-text">
