0ebe27aa48cca761cfb79e17c35f2734b241ede1
Anirban Banerjee <abanerjee763@outlook.com>
2021-05-07 18:13:03 +0530

    Closes #49

--- routes/post.js
+++ routes/post.js
@@ -1,4 +1,4 @@
-const {getPosts, createPost, postsbyuser, postById, isPoster, deletePost, updatePost} = require('../controllers/post');
+const {getPosts, createPost, postsbyuser, postById, isPoster, deletePost, updatePost,postPhoto} = require('../controllers/post');
 const express  = require('express');
 const {createPostValidator} = require('../helpers/index');
 const {userById} = require('../controllers/user');
@@ -13,10 +13,14 @@ router.get("/post/by/:userId", requireSignin, postsbyuser);
 router.delete('/post/:postId', requireSignin, isPoster, deletePost)
 router.put("/post/:postId", requireSignin, updatePost);
 
+
+router.get("/post/photo/:postId", postPhoto);
+
 // any route containing :userId, our app will first execute userbyId
 router.param("userId", userById);
 // any route containing :postId, our app will first execute postbyId
 router.param("postId", postById);
+
 
 module.exports = router;
 // exports.getPosts = (req, res) => {
